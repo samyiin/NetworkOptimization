c324541f2df9c9c7c477dea3fbb6c2e059df6c9e
Sam Yiin <samyiin@Sams-MacBook-Air.local>
2024-06-10 20:55:28 +0300

    completed a flawed version: server will receive some messages, and if it happens to receive twice it will send a message back to client. Clinet will just send 100 message to server and wait for server's respond and close. all the logic is wrong, but the backbone is established

--- Benchmark/client.c
+++ Benchmark/client.c
@@ -11,28 +11,50 @@
 #include <arpa/inet.h>
 #include <sys/time.h>
 
+// must be the same as the server
 #define PORT 8080
 
 void measure_throughput(int sock, int message_size, int num_messages) {
+    // use malloc because we don't know the size of message.
     char *message = (char *)malloc(message_size);
+    // this function will set all the bytes in the allocated memory to be 'A', a char which is one byte.
     memset(message, 'A', message_size);
 
-    struct timeval start, end;
-    double total_time;
 
-    // Send messages
+    // start timer
+    struct timeval start, end;
     gettimeofday(&start, NULL);
+
+    // send multiple messages
     for (int i = 0; i < num_messages; i++) {
+        // send function takes a message as a string (message size because message is a ptr), a socket to send to
+        // The last parameter is flag: 0 -- No Flag.
+        // As documented in connect, here the os knows where to send the message to.
         send(sock, message, message_size, 0);
     }
+
+    printf("sent message\n");
+
+    // recieve result from server
+    // todo: the logic here is flawed, you must keep sending until we read respond from the server.
+    int const BUFFER_SIZE = 1048576 * 2;
+    char buffer[BUFFER_SIZE] = {0};
+    int bytes_read = read(sock, buffer, BUFFER_SIZE);
+    printf("%s", buffer);
+    printf("received message\n");
+
+    // end timer
     gettimeofday(&end, NULL);
+    // calculate time
+    double total_time = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
 
-    total_time = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+    // calculate throughput: afraid of overflow
     double throughput = ((num_messages * message_size) /1000000) / total_time;
-
-    // afraid of overflow
+    // print the throughput
     printf("%d\t%.2f\tMbytes/sec\n", message_size, throughput);
+    // calculate through put
 
+    // free allocated memory
     free(message);
 }
 
@@ -42,40 +64,51 @@ int main(int argc, char const *argv[]) {
         return -1;
     }
 
-    const char *server_ip = argv[1];
-    int sock;
-    struct sockaddr_in serv_addr;
-
-    // Create socket
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
+    // Create client socket: ipv4, TCP
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket < 0) {
+        // todo: close sockets if exit.
         printf("\n Socket creation error \n");
         return -1;
     }
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+    // not setting ip, only set ipv4, port
+    // server_ip will be a string of the ip address of the server, the port will be 8080 by default.
+    const char *server_ip = argv[1];
+    // build the information of the socket we are connecting to
+    struct sockaddr_in serv_addr;
 
-    // Convert IPv4/IPv6 addresses from text to binary form
+    // set ipv4, ip, port
+    serv_addr.sin_family = AF_INET;
     if (inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0) {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
     }
+    serv_addr.sin_port = htons(PORT);
 
-    // Connect to server
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+
+    // Connect to server:
+    // After successfully calling connect, the operating system maintains the connection details for client_socket
+    // So when I later call "send(clent_sever, message, ...), the os would automatically know that I am sending to the
+    // server.
+    if (connect(client_socket, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
         printf("\nConnection Failed \n");
         return -1;
     }
 
-    int message_sizes[] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576};
+    // measure throughput of all of these
+    // int message_sizes[] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576};
+    int message_sizes[] = {1};
     int num_sizes = sizeof(message_sizes) / sizeof(message_sizes[0]);
 
     for (int i = 0; i < num_sizes; i++) {
-        measure_throughput(sock, message_sizes[i], 10000); // Send 1000 messages for each size
+        measure_throughput(client_socket, message_sizes[i], 100); // Send 1000 messages for each size
     }
+    printf("finish everthing, closing client...\n");
 
-    close(sock);
+
+    // close socket: frees up descriptor, for TCP: send the FIN packet.
+    close(client_socket);
     return 0;
 }
 
--- Benchmark/server.c
+++ Benchmark/server.c
@@ -9,14 +9,14 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+#define SERVER_IP "127.0.0.1"   // loopback address: only listen to clients on this machine
+// INADDR_ANY: to bind to all available interface
 #define PORT 8080
-#define BUFFER_SIZE 1048576 // 1MB
 
 int main() {
     int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
-    char buffer[BUFFER_SIZE] = {0};
 
     // AF_INET means it's ipv4, SOCK_STREAM means it's TCP, protocal=0 means automatically select protocal.
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
@@ -38,10 +38,17 @@ int main() {
 
     // specify is it ip4 or ip6 (sin - socket internet, s - struct)
     address.sin_family = AF_INET;
-    // specify the ip address INADDR_ANY: to bind to all available interface
-    // can specify the ip address here: change INADDR_ANY to inet_addr("127.0.0.1");
-    // (to bind to the loopback address： only listen to clients on this machine)
-    address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    // Convert IPv4 addresses from text to binary form (pton: presentation to network)
+    // AF_INET specifies that the address family is IPv4.
+    // src: server_ip: is the string representation of the IP address ("127.0.0.1").
+    // dst: serv_addr.sin_addr: A pointer to a buffer where the function will store the binary representation of the
+    // IP address. If the conversion is successful, "dest".s_addr will contain the binary representation of the
+    // IP address. (It will store in the s_addr attribute of it).
+    // Legacy: address.sin_addr.s_addr = inet_addr(server_ip)
+    if (inet_pton(AF_INET, SERVER_IP, &address.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
     // set port number
     address.sin_port = htons(PORT);
 
@@ -56,6 +63,7 @@ int main() {
     // that is, as a socket that will be used to accept incoming connection requests using the accept function
     // backlog = 3: maximum number of pending connections that can be queued up before the kernel starts to refuse new
     // connection attempts
+    // so all the incomming connections will be put into a queue. Later accept will take a connection from the queue.
     if (listen(server_fd, 3) < 0) {
         perror("listen");
         close(server_fd);
@@ -63,20 +71,50 @@ int main() {
     }
 
     // accept(server_fd,...) is server.accept in oop
-    // besides from server_fd, it takes an empty socket address struct (
-    struct sockaddr_in client_address;
-    int client_addrlen = sizeof(client_address);
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&client_address, (socklen_t*)&client_addrlen)) < 0) {
+    // it will take one connection from the queue above, and create a new socket for that connection. If there is no
+    // connection in the queue, then it will block the process. If the socket is labeled (somehow) "no-blcok" then see
+    // documentation... Why create a new socket? so that the original socket can keep listening, and the new socket is
+    // not labelled as listening, it can send and read message from the client (it's a different kinds of socket
+    // already, Definitely by distinguishing socket and connected socket things become easy to understand.).
+    // https://stackoverflow.com/questions/65912861/why-does-accept-create-a-new-socket
+    // https://stackoverflow.com/questions/50743329/what-does-it-mean-that-accept-creates-a-new-socket
+    // besides from server_fd, it also takes an address pointer (and it's size since it's a pointer)
+    // this points to an "empty" address struct, and later accept function will fill in the client address into the
+    // struct.
+    // This is like the "server version of connect". So later if this new socket wants to send message to the client
+    // socket, the os would know where to send to.
+    struct sockaddr_in client_addr;
+    int client_addrlen = sizeof(client_addr);
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t*)&client_addrlen)) < 0) {
         perror("accept");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
+    printf("received client\n");
 
+    // The read call will block and wait for the client to send data. ( in a block TCP socket)
+    // This means the server will pause execution at the read call until at least one byte of data is available.
     int bytes_read;
-    while ((bytes_read = read(new_socket, buffer, BUFFER_SIZE)) > 0) {
-        // Simulate processing
+    int const BUFFER_SIZE = 1048576; // 2Mb: bigger than any single message from client
+    char buffer[BUFFER_SIZE] = {0};
+    int message_counter = 0;
+    while (1) {
+        bytes_read = read(new_socket, buffer, BUFFER_SIZE);
+        if (bytes_read <= 0){printf("damaged message\n");}
+        // count number of message received; todo: with this buffer method, how to count the number of message received?
+        printf("%d\n", bytes_read);
+        printf("received one message\n");
+        message_counter ++;
+        if (message_counter >= 2){break;}
     }
+    printf("received all messages\n");
 
+    // send a message to the client to indicate that process is completed
+    char *complete_message = "all package recieved";
+    send(new_socket, "all package recieved", strlen("all package recieved"),0);
+    printf("sent messages\n");
+
+    // close both sockets
     close(new_socket);
     close(server_fd);
     return 0;
