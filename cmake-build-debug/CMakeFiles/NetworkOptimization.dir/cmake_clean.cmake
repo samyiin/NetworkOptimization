b9db6b4c73dc3ffab25bc9ebd6afeb6d43e79cc9
Sam Yiin <samyiin@Sams-MacBook-Air.local>
2024-06-11 19:48:40 +0300

    tras

---
+++ cmake-build-debug/CMakeFiles/NetworkOptimization.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NetworkOptimization.dir/Benchmark/server.c.o"
+  "CMakeFiles/NetworkOptimization.dir/Benchmark/server.c.o.d"
+  "NetworkOptimization"
+  "NetworkOptimization.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NetworkOptimization.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
